62a6d0e0f79636cef9597d25b0235a749941a6e1
Viral Sherathiya <viral.sherathiya@corp.capitaworld.com>
2023-05-02 18:17:11 +0530

    updated

--- app.py
+++ app.py
@@ -1,11 +1,51 @@
-import flask
-from flask import Flask
+import os
+import numpy as np
+import threading
+from flask import Flask, render_template, request
+import joblib
+from food_delivery.pipeline.pipeline import Pipeline
+from food_delivery.entity.delivery_time_predictor import DeliveryData, food_deliveryPredictor
+from food_delivery.config.configuration import Configuration
+from food_delivery.constant import *
 
 app = Flask(__name__)
 
 @app.route('/',methods = ['GET','POST'])
 def home():
-    return "Started App"
+    if request.method == 'POST':
+        # Get the form data
+        delivery_person_age = float(request.form.get('Delivery_person_Age'))
+        delivery_person_ratings = float(request.form.get('Delivery_person_Ratings'))
+        restaurant_latitude = float(request.form.get('Restaurant_latitude'))
+        restaurant_longitude = float(request.form.get('Restaurant_longitude'))
+        delivery_location_latitude = float(request.form.get('Delivery_location_latitude'))
+        delivery_location_longitude = float(request.form.get('Delivery_location_longitude'))
+        order_date = request.form.get('Order_Date')
+        time_ordered = request.form.get('Time_Orderd')
+        time_order_picked = request.form.get('Time_Order_picked')
+        weather_conditions = request.form.get('Weather_conditions')
+        road_traffic_density = request.form.get('Road_traffic_density')
+        vehicle_condition = int(request.form.get('Vehicle_condition'))
+        type_of_order = request.form.get('Type_of_order')
+        type_of_vehicle = request.form.get('Type_of_vehicle')
+        multiple_deliveries = float(request.form.get('multiple_deliveries'))
+        festival = request.form.get('Festival')
+        city = request.form.get('City')
+        
+        # Perform any required processing on the form data here
+        food_delivery_data = DeliveryData(longitude=longitude,
+                                   latitude=latitude,
+                                   food_delivery_median_age=food_delivery_median_age,
+                                   total_rooms=total_rooms,
+                                   total_bedrooms=total_bedrooms,
+                                   population=population,
+                                   households=households,
+                                   median_income=median_income,
+                                   ocean_proximity=ocean_proximity)
+        # Return a response
+        return 'Form submitted successfully!'
+    
+    return render_template('form.html')
 
 
 @app.route('/train',methods = ['GET','POST'])
@@ -17,6 +57,19 @@ def training():
 def predict():
     return "Started Predicting"
 
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+    
+
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
+    
+    
+    
+    
--- food_delivery/entity/delivery_time_predictor.py
+++ food_delivery/entity/delivery_time_predictor.py
@@ -10,28 +10,48 @@ import pandas as pd
 class DeliveryData:
 
     def __init__(self,
-                 longitude: float,
-                 latitude: float,
-                 food_delivery_median_age: float,
-                 total_rooms: float,
-                 total_bedrooms: float,
-                 population: float,
-                 households: float,
-                 median_income: float,
-                 ocean_proximity: str,
-                 median_house_value: float = None
+                 delivery_person_age: float,
+                 delivery_person_ratings: float,
+                 restaurant_latitude: float,
+
+                 restaurant_longitude: float,
+
+                 delivery_location_latitude: float,
+
+                 delivery_location_longitude: float,
+                 order_date : object,
+                 time_ordered : object,
+                 time_order_picked : object,
+                 weather_conditions : object,
+                 road_traffic_density  : object,
+                 vehicle_condition  : int,
+                 type_of_order : object,
+                 type_of_vehicle : object,
+                 multiple_deliveries :float,
+                 festival : object,
+                 city : object
                  ):
         try:
-            self.longitude = longitude
-            self.latitude = latitude
-            self.food_delivery_median_age = food_delivery_median_age
-            self.total_rooms = total_rooms
-            self.total_bedrooms = total_bedrooms
-            self.population = population
-            self.households = households
-            self.median_income = median_income
-            self.ocean_proximity = ocean_proximity
-            self.median_house_value = median_house_value
+            self.delivery_person_age = delivery_person_age
+            self.delivery_person_ratings = delivery_person_ratings
+            self.restaurant_latitude = restaurant_latitude
+            self.restaurant_longitude = restaurant_longitude
+            self.delivery_location_latitude = delivery_location_latitude
+            self.delivery_location_longitude = delivery_location_longitude
+            self.order_date = order_date
+            self.time_ordered = time_ordered
+            self.time_order_picked = time_order_picked
+            self.weather_conditions = weather_conditions
+            self.road_traffic_density = road_traffic_density
+            self.vehicle_condition = vehicle_condition
+            self.type_of_order = type_of_order
+            self.type_of_vehicle = type_of_vehicle
+            self.multiple_deliveries = multiple_deliveries
+            self.festival = festival
+            self.city = city
+            
+        
+            
         except Exception as e:
             raise CustomException(e, sys) from e
 
@@ -46,15 +66,23 @@ class DeliveryData:
     def get_food_delivery_data_as_dict(self):
         try:
             input_data = {
-                "longitude": [self.longitude],
-                "latitude": [self.latitude],
-                "food_delivery_median_age": [self.food_delivery_median_age],
-                "total_rooms": [self.total_rooms],
-                "total_bedrooms": [self.total_bedrooms],
-                "population": [self.population],
-                "households": [self.households],
-                "median_income": [self.median_income],
-                "ocean_proximity": [self.ocean_proximity]}
+                "delivery_person_age": [self.delivery_person_age],
+                "delivery_person_ratings": [self.delivery_person_ratings],
+                "restaurant_latitude": [self.restaurant_latitude],
+                "restaurant_longitude": [self.restaurant_longitude],
+                "delivery_location_latitude": [self.delivery_location_latitude],
+                "delivery_location_longitude": [self.delivery_location_longitude],
+                "order_date": [self.order_date],
+                "time_ordered": [self.time_ordered],
+                "time_order_picked": [self.time_order_picked],
+                "weather_conditions": [self.weather_conditions],
+                "road_traffic_density": [self.road_traffic_density],
+                "type_of_order": [self.type_of_order],
+                "vehicle_condition": [self.vehicle_condition],
+                "type_of_vehicle": [self.type_of_vehicle],
+                "multiple_deliveries": [self.multiple_deliveries],
+                "festival": [self.festival],
+                "city": [self.city]}
             return input_data
         except Exception as e:
             raise CustomException(e, sys)
@@ -71,7 +99,8 @@ class food_deliveryPredictor:
     def get_latest_model_path(self):
         try:
             folder_name = list(map(int, os.listdir(self.model_dir)))
-            latest_model_dir = os.path.join(self.model_dir, f"{max(folder_name)}")
+            latest_model_dir = os.path.join(
+                self.model_dir, f"{max(folder_name)}")
             file_name = os.listdir(latest_model_dir)[0]
             latest_model_path = os.path.join(latest_model_dir, file_name)
             return latest_model_path
